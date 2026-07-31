1c9c3ab603b3e3b462b01ca78c1da12702db25ca
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-07-31 21:30:52 +0530

    p3

---
+++ C programming/p2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x = 15 , y = 6;
+    printf("Sum = %d\n",x+y);
+    printf("difference = %d\n",x-y);
+    printf("product = %d\n",x*y);
+    printf("quotient = %d\n",x/y);
+    printf("Remainder = %d\n",x%y);
+    return 0;
+    
+}
---
+++ C programming/p3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main ()
+{
+    float x = 5.8,y = 2.3;
+    printf ("sum = %f\n",x+y);
+    printf ("difference = %f\n",x-y);
+    printf ("product = %f\n",x*y);
+    printf ("quotient = %f\n",x/y);
+    printf ("quotient = %.3f\n",x/y);
+    return 0;
+
+}
