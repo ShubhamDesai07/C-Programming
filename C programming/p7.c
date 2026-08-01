34366f01b41a8b200babb3392844b3c674804074
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-08-01 21:14:57 +0530

    comma operator

---
+++ C programming/p7.c
@@ -0,0 +1,18 @@
+// comma as separator and operator
+
+
+#include<stdio.h>
+
+int main ()
+{
+    int a,b;
+    a = (2,3,4) ; 
+
+    b = 2,3,4 ;
+
+    printf("%d,%d",a,b);
+
+    return 0 ;
+
+
+}
