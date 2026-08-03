48943293d7a60903e579953008a1928e0b3bcc2e
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-08-03 22:53:28 +0530

    case 3

---
+++ C programming/p16.c
@@ -0,0 +1,20 @@
+//cases of typecasting
+
+
+
+//case 3
+
+#include<stdio.h>
+
+int main ()
+
+{
+    int a = 10 , b = 3 ;
+    float c =  (float)(a/b);
+    printf("%f",c);
+    return 0 ;
+
+    
+
+
+}
