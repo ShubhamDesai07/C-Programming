01504995176e3a2d81b30a6f9efd630d70424bb8
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-08-03 22:43:39 +0530

    explicit typecasting

---
+++ C programming/p13.c
@@ -0,0 +1,18 @@
+//type casting 
+
+//explicit type casting
+
+
+#include<stdio.h>
+
+int main ()
+
+{
+    float x = 3.435;
+    int y = (int)x;
+    printf("%f\n",x);
+    printf("%d",y);
+
+    return 0;
+    
+}
