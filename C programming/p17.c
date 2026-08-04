0b5d60e61bf9f7d0a517115a3dfec357d03508c7
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-08-04 20:00:37 +0530

    if else

---
+++ C programming/p17.c
@@ -0,0 +1,18 @@
+// if else
+
+#include<stdio.h>
+
+void main ()
+
+{
+    int num1 , num2 ;
+    scanf ("%d,%d",&num1,&num2);
+    if (num1>num2)
+       {
+        printf("%d is greater ",num1);
+       }   
+    else
+        {
+        printf("%d is greater ",num2);
+        }
+}    
