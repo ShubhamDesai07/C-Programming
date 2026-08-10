80f85580246f2a6a7196fe216c7f461c9cfc8b6d
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-08-10 20:33:26 +0530

    Find the largest of two numbers

---
+++ C programming/p25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main (){
+    int n1,n2;
+
+    printf("Enter a number :\n");
+    scanf("%d",&n1);
+
+    printf("Enter a number :");
+    scanf("%d",&n2);
+
+    if (n1>n2)
+        printf("%d is greater",n1);
+    else
+        printf("%d is greater",n2);
+
+
+
+}
